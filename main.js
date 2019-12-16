4291aeb9ce2f379b179e39ce8f0ee792557651e1
kaminora <kaminoura9207@gmail.com>
2019-12-17 03:06:16 +0900

    Introduce Navigo for router

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-import canvas from './canvas.js';
+import Navigo from 'navigo';
+import Drawer from './drawer.js';
+import Canvas from './canvas';
+
+var root = null;
+var useHash = false;
+var hash = '#';
+var router = new Navigo(root, useHash, hash);
+
+router
+  .on('/input', function () {
+    console.log('show input')
+  })
+  .on('/', function () {
+    console.log('show canvas')
+    const drawer = new Drawer();
+    drawer.drawText();
+  })
+  .resolve();
