fcd9420fb900c485ccdbb44bb271fac2192300aa
kaminora <kaminoura9207@gmail.com>
2019-12-19 08:36:31 +0900

    :+1: Move filled text from right to left

--- src/drawer.js
+++ src/drawer.js
@@ -1,14 +1,24 @@
 function Drawer() {
   this.canvas = document.getElementById('canvas');
   this.ctx = this.canvas.getContext('2d');
+  this.ctx.font = "24px Arial";
   this.ctx.strokeStyle = "#323232";
   this.ctx.lineWidth = 2; 
 }
 
 Drawer.prototype = {
   drawText: function (text) {
-    this.ctx.font = "24px Arial";
-    this.ctx.fillText(text, this.canvas.width / 2, this.canvas.height / 2)
+    const self = this;
+    const intervalId = setInterval(function () {
+      let posX = this.canvas.width;
+      self.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      self.ctx.fillText(text, posX, this.canvas.height / 2)
+      posX -= 10
+      // todo: calculate text width
+      if (posX < -1000) {
+        this.clearInterval(intervalId)
+      }
+    }, 20 )
   }
 }
 
