06135b1f3fd3ff327b3db1fac461af23844a4668
kaminora <kaminoura9207@gmail.com>
2019-12-19 17:52:43 +0900

    :recycle: Refactor function

--- src/drawer.js
+++ src/drawer.js
@@ -14,21 +14,17 @@ Drawer.prototype = {
   drawText: function(text, speed) {
     const self = this;
     window.requestAnimationFrame(function() {
-      self._drawText(text, speed)
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.counter += 2 * speed;
+      const offsetX = this.posX - this.counter;
+      this.ctx.fillText(text, offsetX, this.posY);
+      // todo: calculate text width
+      if (offsetX < -400) {
+        return;
+      }
+      this.drawText(text, speed);
     })
   },
-  _drawText: function (text, speed) {
-    console.log(speed)
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.counter += 2 * speed;
-    const offsetX = this.posX - this.counter;
-    this.ctx.fillText(text, offsetX, this.posY);
-    // todo: calculate text width
-    if (offsetX < -400) {
-      return;
-    }
-    this.drawText(text, speed);
-  }
 }
 
 export default Drawer;
